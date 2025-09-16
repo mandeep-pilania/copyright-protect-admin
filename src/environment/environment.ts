5055d87bbddfc367b6cac32e046cbb21420eea10
vkjaatji <vkarwasra127@gmail.com>
2025-09-17 00:22:07 +0530

    admin all done

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -9,5 +9,13 @@ export const environment = {
     messagingSenderId: '833889802715',
     appId: '1:833889802715:web:6ee4eff2de9c811d416ffe',
     measurementId: 'G-J5TMFD1X0L',
+
+    //    apiKey: 'AIzaSyDKO1dmr0uLbe6U941vwbm0JLL8R1tpAtc',
+    // authDomain: 'copyright-protect.firebaseapp.com',
+    // projectId: 'copyright-protect',
+    // storageBucket: 'copyright-protect.firebasestorage.app',
+    // messagingSenderId: '826070411918',
+    // appId: '1:826070411918:web:626bcc20d5ab1be50877df',
+    // measurementId: 'G-2GPHBFR6S0',
   },
 };
